abbd006fbe7a097216efc641a41bc0a11b54fa7b
Eleniitha <princess160595@hotmail.com>
2014-09-25 19:40:32 -0500

    primer commit

---
+++ hola mundo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ALUMNOS
+ * Date: 25/09/2014
+ * Time: 07:39 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace hola_mundo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("hola mundo");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
